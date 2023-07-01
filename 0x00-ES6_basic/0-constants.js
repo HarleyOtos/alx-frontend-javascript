94a0c102effdc4acb430c7a50d610949883dace8
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-07-01 20:51:35 +0100

    flies & config update

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,15 @@
 export function taskFirst() {
-    const task = 'I prefer const when I can.';
-    return task;
+  const task = 'I prefer const when I can.';
+  return task;
 }
 
 export function getList() {
-    return ' is okay';
+  return ' is okay';
 }
 
 export function taskNext() {
-    let combination = 'But sometimes let';
-    combination += getList();
+  let combination = 'But sometimes let';
+  combination += getList();
 
-    return combination;
+  return combination;
 }
