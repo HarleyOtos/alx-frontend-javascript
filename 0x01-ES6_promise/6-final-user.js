5a3ecafbd130eb17db196a0a9ff6e9dad9effaf9
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-08-03 19:16:03 +0100

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,13 +1,16 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, filename) {
+export default function handleProfileSignup(
+  firstName, lastName, filename,
+) {
   const signUpPromise = signUpUser(firstName, lastName);
   const uploadPromise = uploadPhoto(filename);
 
   return Promise.allSettled([signUpPromise, uploadPromise])
     .then((results) => results.map((result) => ({
       status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason.message,
+      value: result.status === 'fulfilled'
+        ? result.value : result.reason.message,
     })));
 }
