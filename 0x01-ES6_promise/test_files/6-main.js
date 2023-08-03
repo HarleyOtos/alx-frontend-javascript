c1448a4aa48bff7bf0dc4d6dbb2ad14404935e61
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-08-03 20:07:27 +0100

    test files

---
+++ 0x01-ES6_promise/test_files/6-main.js
@@ -0,0 +1,9 @@
+import handleProfileSignup from './6-final-user';
+
+console.log(handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg"));
+
+// To see the result
+
+// handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg").then((results) => {
+//   console.log(results);
+// });
