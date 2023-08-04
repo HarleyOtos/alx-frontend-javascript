45f9e4e24b55f085f861191f8cfb59817461e1f6
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-08-04 20:58:59 +0100

    Code fix

---
+++ 0x02-ES6_classes/9-hoisting.js
@@ -0,0 +1,50 @@
+export class HolbertonClass {
+  constructor(year, location) {
+    this._year = year;
+    this._location = location;
+  }
+
+  // Getter for year
+  get year() {
+    return this._year;
+  }
+
+  // Getter for location
+  get location() {
+    return this._location;
+  }
+}
+
+export class StudentHolberton {
+  constructor(firstName, lastName, holbertonClass) { // Add the missing holbertonClass parameter
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._holbertonClass = holbertonClass; // Assign holbertonClass correctly
+  }
+
+  // Getter for fullName
+  get fullName() {
+    return `${this._firstName} ${this._lastName}`;
+  }
+
+  // Getter for holbertonClass
+  get holbertonClass() {
+    return this._holbertonClass;
+  }
+
+  // Getter for fullStudentDescription
+  get fullStudentDescription() {
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`; // Use `this`, not `self`
+  }
+}
+
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
+
+const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
+const student2 = new StudentHolberton('John', 'Doe', class2020);
+const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+const student4 = new StudentHolberton('Donald', 'Bush', class2019);
+const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+
+export const listOfStudents = [student1, student2, student3, student4, student5];
