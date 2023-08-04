fd1943edfb132797af6b9b5dce5e22ae3e3678ac
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-08-04 21:50:59 +0100

    Implement a class named EVCar

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,19 @@
+import Car from './10-car.js';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  cloneCar() {
+    const clonedCar = new Car();
+    const symbols = Object.getOwnPropertySymbols(this);
+
+    for (const symbol of symbols) {
+      clonedCar[symbol] = this[symbol];
+    }
+
+    return clonedCar;
+  }
+}
