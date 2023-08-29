644071d62f2d6b31932260984847445b7f5e72cf
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-08-29 21:07:25 +0100

    task 1 extending class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,6 +7,7 @@ interface Teacher {
   [key: string]: any;
 }
 
+// Task 1: Teacher interface implemented
 const teacher3: Teacher = {
   firstName: 'Edward',
   lastName: 'Okafor',
@@ -15,4 +16,43 @@ const teacher3: Teacher = {
   contract: false,
 };
 
-console.log(teacher3);
+// Task 2: Extends the teacher class
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// Task 3: printing teachers
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  const firstInitial = firstName.charAt(0).toUpperCase();
+  const formattedLastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
+
+  return `${firstInitial}. ${formattedLastName}`;
+}
+
+// Interface for StudentClass constructor
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClass;
+}
+
+// Interface for the StudentClass instance
+interface StudentClass {
+  workOnHomeWork(): string;
+  displayName(): string;
+}
+
+// Implementing the StudentClass
+class Student implements StudentClass {
+  constructor(private firstName: string, private lastName: string) {}
+
+  workOnHomeWork(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
